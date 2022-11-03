34a459b35b3731552c52c31dc5a0ddab8ac32462
Eric Wang <eric0717@gmail.com>
2022-11-02 21:56:15 -0500

    Added tooltip

--- myscript.js
+++ myscript.js
@@ -1,9 +1,29 @@
+$(document).ready(function () {
+
+
 $(document).on("mouseenter", "span.instructors", function(){
 
-    alert($(this).text());
+    
+    
+    var wrapper = "<b class='tooltip'><span>u are hovering rn</span></b>";
+          $(this).after(wrapper);
+          
+         
 
+    
     //instead of an alert make a box somewhere 
     //also get the parent doms of the instructor -> class name, subject/dept
-
+    
     //return the link of the search of the course evals
 });
+
+$(document).on("mouseleave", "span.instructors", function(){
+
+    var remove = document.querySelector('b.tooltip');
+    remove.parentElement.removeChild(remove);
+
+})
+
+})
+
+
