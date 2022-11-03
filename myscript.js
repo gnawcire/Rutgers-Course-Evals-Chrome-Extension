ebd8496b728463a1c4eab5d8652a058f8ebd885a
Eric Wang <eric0717@gmail.com>
2022-11-02 19:07:02 -0500

    Initialized Chrome Extension

---
+++ myscript.js
@@ -0,0 +1,9 @@
+$(document).on("mouseenter", "span.instructors", function(){
+
+    alert($(this).text());
+
+    //instead of an alert make a box somewhere 
+    //also get the parent doms of the instructor -> class name, subject/dept
+
+    //return the link of the search of the course evals
+});
