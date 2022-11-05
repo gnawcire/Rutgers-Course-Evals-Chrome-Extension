68c91901f58015b86f7acac164fb1c953eed48cb
Eric Wang <eric0717@gmail.com>
2022-11-04 23:50:08 -0500

    Special Cases for Prof Names

--- myscript.js
+++ myscript.js
@@ -4,18 +4,45 @@ $(document).on("mouseenter", "span.instructors", function(){
     var spanInstructors = document.getElementsByClassName('instructors');
     console.log(spanInstructors);
     
+
+    //in case there are multiple professors listed in span.instructors
     var instructorName = $(this).text();
+    var actualInstructor = instructorName;
+    for (let i =0; i < instructorName.length; i++){
+        if (instructorName.substring(i, i+1) == ';') {
+            actualInstructor = instructorName.substring(0, i);
+            break;
+        }
+
+    }
+    for (let i = 0; i < actualInstructor.length; i++){
+        if(actualInstructor.substring(i, i+1) == ','){
+            
+            if (actualInstructor.substring(0,i).length < 3){
+                actualInstructor = actualInstructor.substring(i+2);
+            } 
+            else actualInstructor = actualInstructor.substring(0, i);
+        }
+    }
+    //get rid of comma, do first / last name, 
+
     console.log(instructorName);
+    console.log(actualInstructor);
     var parentId = $(this).closest('span.instructors').parent().attr('id');
     var classId = parentId.substring(0,10);
     var school = parentId.substring(0, 2);
     var subject = parentId.substring(3, 6);
     var className = parentId.substring(7, 10);
 
-    var link = "https://sirs.ctaar.rutgers.edu/index.php?mode=name&survey%5Blastname%5D=" + instructorName + "&survey%5Bsemester%5D=&survey%5Byear%5D=&survey%5Bschool%5D=" + school + "&survey%5Bdept%5D=" + subject;
+    var link = "https://sirs.ctaar.rutgers.edu/index.php?mode=name&survey%5Blastname%5D=" + actualInstructor + "&survey%5Bsemester%5D=&survey%5Byear%5D=&survey%5Bschool%5D=" + school + "&survey%5Bdept%5D=" + subject;
+    $(this).attr('onclick', "window.location='https://sirs.ctaar.rutgers.edu/index.php?mode=name&survey%5Blastname%5D=" + actualInstructor + "&survey%5Bsemester%5D=&survey%5Byear%5D=&survey%5Bschool%5D=" + school + "&survey%5Bdept%5D=" + subject+ "'")
+
+    //above is the stuff for course evals, below will be for ratemyprofessor
+    var rateMyProfessorLink = ""
+    if(document.querySelector('b.tooltip') == null){
     var wrapper = "<b class='tooltip'><span>" + classId + "</span></b>";
           $(this).after(wrapper);
-   $(this).attr('onclick', "window.location='https://sirs.ctaar.rutgers.edu/index.php?mode=name&survey%5Blastname%5D=" + instructorName + "&survey%5Bsemester%5D=&survey%5Byear%5D=&survey%5Bschool%5D=" + school + "&survey%5Bdept%5D=" + subject+ "'")
+    }
     
    
    
@@ -23,13 +50,17 @@ $(document).on("mouseenter", "span.instructors", function(){
     // spanInstructors.href = "https://sirs.ctaar.rutgers.edu/index.php?mode=name&survey%5Blastname%5D=" + instructorName + "&survey%5Bsemester%5D=&survey%5Byear%5D=&survey%5Bschool%5D=" + school + "&survey%5Bdept%5D=" + subject;
     
     //to do: return the link of the search of the course evals
+    
 });
 
 
 $(document).on("mouseleave", "span.instructors", function(){
-    //bug: will create multiple if u leave span.instructors but stay in div.sectionData
-    var remove = document.querySelector('b.tooltip');
-    remove.parentElement.removeChild(remove);
+    setTimeout(function() {
+        if (document.querySelector('b.tooltip') != null){
+        var remove = document.querySelector('b.tooltip');
+        remove.parentElement.removeChild(remove);
+        }
+    }, 3000)
     
 
 })
