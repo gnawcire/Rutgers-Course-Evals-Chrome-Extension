f431430cc02a912283d5642a9d8031d09bb93fef
nataliechoww <116742098+nataliechoww@users.noreply.github.com>
2022-11-09 21:00:20 -0500

    updated background script

--- backgroundScript.js
+++ backgroundScript.js
@@ -1,8 +1,11 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    fetch(request.url, {mode:'no-cors'})
-
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+    fetch(request.url, {mode: 'cors'}) 
     .then(response => response.text())
-    .then(text =>  sendResponse({returned_text:text}))
-    return true;
-
-})    
+    .then(text => {
+        sendResponse({responseText: ratingValue});
+     })
+     .catch(error => console.log(error));
+     return true;
+     }    
+);
