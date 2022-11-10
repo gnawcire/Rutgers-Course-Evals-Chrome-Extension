46e7886545ef9c99ed9031da2043423061b49b2b
nataliechoww <116742098+nataliechoww@users.noreply.github.com>
2022-11-09 21:30:12 -0500

    split text

--- backgroundScript.js
+++ backgroundScript.js
@@ -3,7 +3,10 @@ chrome.runtime.onMessage.addListener(
     fetch(request.url, {mode: 'cors'}) 
     .then(response => response.text())
     .then(text => {
-        sendResponse({responseText: ratingValue});
+        const split = text.split("avgRating");
+        let resptext = split[1].toString(); // only returns 1st rating
+        console.log(resptext);
+        sendResponse({responseText: resptext});
      })
      .catch(error => console.log(error));
      return true;
